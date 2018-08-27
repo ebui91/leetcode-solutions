eb417443d90de20262fc43b2ef6961b27cb2a38e
Eric Bui <ericbui2@gmail.com>
2018-08-27 16:45:57 -0500

    [refactor] I forgot how SQL comments work XD
    [refactor] I forgot how SQL comments work XD

--- Easy/Big Countries.sql
+++ Easy/Big Countries.sql
@@ -4,23 +4,23 @@
 */
 
 
-// Level 1 Solution: Using WHERE clause with OR
+/* Level 1 Solution: Using WHERE clause with OR */
 SELECT name, population, area FROM World WHERE area > 3000000 OR population > 25000000
 
-// Alternatively, separate code for cleaner and more readable syntax:
+/* Alternatively, separate code for cleaner and more readable syntax: */
 SELECT name, population, area 
 FROM World
 WHERE area > 3000000
 OR population > 25000000
 
 
-// Level 2 Solution: Using a UNION
-// This solution is slightly faster than the previous one.
+/* Level 2 Solution: Using a UNION */
+/* This solution is slightly faster than the previous one. */
 SELECT name, population, area FROM World WHERE area > 3000000
 UNION
 SELECT name, population, area FROM World WHERE population > 25000000
 
-// Alternative syntax:
+/* Alternative syntax: */
 SELECT name, population, area 
 FROM World 
 WHERE area > 3000000
